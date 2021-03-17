93353f1c57b18eb8d12f46a22844bdef7b3a4a20
Mario Franze <mario.franze@gmail.com>
2021-03-17 06:48:06 +0100

    Added some logic

--- client/src/components/ToDo.vue
+++ client/src/components/ToDo.vue
@@ -1,5 +1,13 @@
 <template>
   <div id="container">
+    <div class="row mb-4">
+      <div class="col-sm"></div>
+      <div class="col-sm-9 text-end">
+        <button type="button" class="btn btn-primary me-3">Add ToDo</button>
+        <button type="button" class="btn btn-danger" @click="logout">Logout</button>
+      </div>
+      <div class="col-sm"></div>
+    </div>
     <div class="row">
       <div class="col-sm"></div>
       <div class="col-sm-9">
@@ -13,7 +21,12 @@
             <th>Town</th>
           </tr>
           <tbody>
-            <tr v-for="todo in todoList" :key="todo.id" :class="{ strikeout: todo.isFinished === true }">
+            <tr
+              v-for="todo in todoList"
+              :key="todo.id"
+              :class="{ strikeout: todo.isFinished === true }"
+              @click="toggleToDo(todo.id)"
+            >
               <td>{{ todo.id }}</td>
               <td>{{ todo.terminalId }}</td>
               <td>{{ todo.branch }}</td>
@@ -53,6 +66,35 @@ export default {
     this.updateTodoList();
   },
   methods: {
+    clearCredentials() {
+      localStorage.id = -1;
+      localStorage.token = '';
+      this.$parent.token = '';
+    },
+    logout() {
+      localStorage.clear();
+      this.$parent.token = '';
+    },
+    toggleToDo(id) {
+      this.axios
+        .put(
+          `${this.$parent.API_URL}/todo/toggle/${id}`,
+          {},
+          {
+            headers: { token: this.$parent.token }
+          }
+        )
+        .then((res) => {
+          clearInterval(this.todoUpdateTimer);
+          const item = this.todoList.find((item) => item.id == id);
+          if (item) item.isFinished = !item.isFinished;
+          this.todoUpdateTimer = setTimeout(this.updateTodoList, 1000);
+        })
+        .catch((err) => {
+          const response = err.response.data;
+          if (response.Code === 403) return this.clearCredentials();
+        });
+    },
     updateTodoList() {
       this.axios
         .get(`${this.$parent.API_URL}/todo/list/${this.showFinished ? 'showfinished' : ''}`, {
@@ -60,27 +102,35 @@ export default {
         })
         .then((res) => {
           this.todoList = res.data.Data.ToDos;
-          setTimeout(this.updateTodoList, 1000);
+          this.todoUpdateTimer = setTimeout(this.updateTodoList, 1000);
         })
         .catch((err) => {
           const response = err.response.data;
-          if (response.Code === 403) return (this.$parent.token = '');
-          setTimeout(this.updateTodoList, 1000);
-        })
-        .finally(() => {});
+          if (response.Code === 403) return this.clearCredentials();
+
+          this.todoUpdateTimer = setTimeout(this.updateTodoList, 1000);
+        });
     }
+  },
+  beforeDestroy() {
+    clearInterval(this.todoUpdateTimer);
   }
 };
 </script>
 
 <style scoped>
-body {
-  overflow-x: hidden;
-}
 table {
   border-collapse: collapse;
 }
-
+tr {
+  cursor: pointer;
+}
+tr:first-child {
+  cursor: default;
+}
+tr:hover {
+  background-color: rgba(0, 0, 0, 0.1);
+}
 td {
   position: relative;
   padding: 5px 10px;
@@ -93,5 +143,9 @@ tr.strikeout td:before {
   left: 0;
   border-bottom: 1px solid #111;
   width: 100%;
+}
+
+.row {
+  --bs-gutter-x: 0;
 }
 </style>
